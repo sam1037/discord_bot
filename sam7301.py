4d958ad11b556e1e894313f37222d65f1b21023e
sam1037 <39227131+sam1037@users.noreply.github.com>
2020-02-05 14:50:29 +0800

    cleared some unuse code

--- sam7301.py
+++ sam7301.py
@@ -5,11 +5,10 @@ import asyncio
 
 client = commands.Bot(command_prefix=".")
 allow_spam = True
-number, ans, play_guess_num, play_millionaire = 0, 0, 0, 0
+number, ans, play_guess_num = 0, 0, 0
 guess_count = 'None'
 messages_list = []
 
-million_questions = [['q1. 1+1 = ?', ['a:2', 'b:1', 'c:3', 'd:4']]]
 @client.event
 async def on_ready():
     open('messages.txt', 'w').close()  # clear the messages.txt file
@@ -48,18 +47,6 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(replies)}')
 
 
-song_ban_list = []  # todo keep it from being blank every time I run this bot
-
-
-@client.command(aliases=['bansong', 'bs'])
-async def ban_song(ctx, song_name):
-    """this func is under development, not working at the moment"""
-    await ctx.send(f'{song_name} has been banned')
-    global song_ban_list
-    song_ban_list.append(song_name)
-    print(song_ban_list)
-
-
 @client.command(aliases=['stopspam'])
 async def stop_spam(ctx):
     """.stopspam, this ban people from using the func 'spam'"""
@@ -86,23 +73,12 @@ async def spam(ctx, time, *args):
 
 
 @client.command(aliases=['rannum'])
-async def ran_num_gen(ctx, start, end):
+async def ran_num_gen(ctx, start=1, end=10):
     """.rannum <start> <end>, this allow you to generate a random number in a range"""
     await ctx.send(f'{random.randint(int(start),int(end))}')
 
 
-@client.command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
-
-@client.command()
-async def leave(ctx):
-    await ctx.voice_client.disconnect()
-
-
-@client.command(aliases=['playguessnum'])
+@client.command(aliases=['playguessnum', 'pgn'])
 async def guess_num(ctx, num=100):
     global ans, play_guess_num, guess_count
     number = int(num)
@@ -112,30 +88,8 @@ async def guess_num(ctx, num=100):
     await ctx.send("Enter number to guess.")
 
 
-@client.command(aliases=['playmillionaire', 'playmil'])
-async def millionaire(ctx):
-    global play_millionaire, mil_ans
-    play_millionaire = True
-
-    question_set = random.choice(million_questions)
-    mil_choices = question_set[1]
-    random.shuffle(mil_choices)
-    mil_ans = mil_choices.index(question_set[1][0])
-
-    await ctx.send(question_set[0])
-    await ctx.send(f'A: {mil_choices[0]}')
-    await ctx.send(f'B: {mil_choices[1]}')
-    await ctx.send(f'C: {mil_choices[2]}')
-    await ctx.send(f'D: {mil_choices[3]}')
-
-
 @client.event
 async def on_message(msg):
-
-    if '-play' in msg.content.split():
-        await msg.channel.send("-stop")
-        await msg.channel.send("Why doesn't this work on groovy??")
-        # todo make groovy stop
 
     # guess num
     global play_guess_num, guess_count
@@ -157,28 +111,6 @@ async def on_message(msg):
                 else:
                     await msg.channel.send(f"You win! You have guessed {guess_count+1} times.")
                 play_guess_num = False
-
-    global play_millionaire, mil_ans
-    if play_millionaire:
-        if msg.content in ['a', 'b', 'c', 'd', 'A', 'B', 'C', 'D']:
-            if mil_ans == 0:
-                if msg.content == 'a' or 'A':
-                    await msg.channel.send('You are right.')
-                    mil_continue = True
-            if mil_ans == 1:
-                if msg.content == 'b' or 'B':
-                    await msg.channel.send('You are right.')
-                    mil_continue = True
-            if mil_ans == 2:
-                if msg.content == 'c' or 'C':
-                    await msg.channel.send('You are right.')
-                    mil_continue = True
-            if mil_ans == 3:
-                if msg.content == 'd' or 'D':
-                    await msg.channel.send('You are right.')
-                    mil_continue = True
-                    
-
 
     await client.process_commands(msg)
 
