6b11a77c322fa4d040c8a3934adae7229571ce87
sam1037 <samngai2522@gmail.com>
2021-10-23 14:10:56 +0800

    added reminder command

--- sam7301.py
+++ sam7301.py
@@ -1,13 +1,25 @@
 #todo better orgainise the code using cogs, see more in yt tutorial.
 
+from asyncio.tasks import wait
+import re
+from types import DynamicClassAttribute
 import discord
 from discord.ext import commands, tasks
 import random
 import asyncio
 import math
-from datetime import datetime
+import datetime
+import time
+import csv
+from dateutil import parser
+import sqlite3
 
-client = commands.Bot(command_prefix=".")
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=".", intents=intents)
+client = discord.Client()
+
+
 allow_spam = True
 number, ans, play_guess_num = 0, 0, 0
 guess_count = 'None'
@@ -35,33 +47,34 @@ num_emo_unicode = [
     "9\ufe0f\u20e3"
 ]
 
-client.remove_command("help")
-
-def get_roast_sent():
-    f = open("roast_you.txt", "r")
-    lines = f.readlines()
-    sent = random.choice(lines)
-    f.close()
-    return sent
+bot.remove_command("help")
 
 
-@client.event
+#set up database
+conn = sqlite3.connect("bot.db")
+cursor = conn.cursor()
+
+
+@bot.event
 async def on_ready():
     printTime.start()
     global nine_chat
-    for channel in client.get_all_channels():
+    for channel in bot.get_all_channels():
         if channel.name == "9chat":
             nine_chat = channel
     open('messages.txt', 'w').close()  # clear the messages.txt file
     print("Bot is ready")
     # todo schedule that task using schedule
 
-    a = [a for a in client.all_commands]
+    a = [a for a in bot.all_commands]
     for a1 in a:
         print(a1)
 
+    #check reminder ?is there better way to implement this?
+    check_reminder.start()
 
-@client.command()
+
+@bot.command()
 async def get_id(ctx):
     # get the voice channel id
     if ctx.author.voice and ctx.author.voice.channel:
@@ -75,11 +88,11 @@ async def get_id(ctx):
 
 @tasks.loop(hours=1)
 async def printTime():
-    now = datetime.now()
+    now = datetime.datetime.now()
     current_time = now.strftime("%d:%H")
     print("Current Time =", current_time)
 
-@client.command()
+@bot.command()
 async def help(ctx):
     helpEmbed = discord.Embed(color=ctx.author.color, title="HELP")
 
@@ -91,10 +104,11 @@ async def help(ctx):
     helpEmbed.add_field(name="allowspam", value="Allow the command 'spam' to be used", inline=False)
     helpEmbed.add_field(name="spam <time> <content>", value="Spam the content for a number of time", inline=False)
     helpEmbed.add_field(name="guessnum <maxium number>", value="Play a game of guessing number", inline=False)
+    helpEmbed.add_field(name="remind", value="send a reminder after some time", inline=False)
 
     await ctx.message.channel.send(embed=helpEmbed)
 
-@client.command()
+@bot.command()
 async def split(ctx):
     # get channel id
     try:
@@ -113,7 +127,7 @@ async def split(ctx):
         teamB = [a.display_name for a in mem_list if a not in teamA]
         teamA = [a.display_name for a in teamA]
 
-    #create embed object and send result
+    #create embed object and send time_match
         mesA = discord.Embed(
             title = "TEAM A",
             colour = discord.Colour.red(),
@@ -129,7 +143,7 @@ async def split(ctx):
         await ctx.message.channel.send(embed=mesA)
         await ctx.message.channel.send(embed=mesB)
 
-@client.command()
+@bot.command()
 async def pick(ctx, num=1):
     num = int(num)
     # get channel id
@@ -154,13 +168,13 @@ async def pick(ctx, num=1):
         await ctx.message.channel.send(embed=mes)
 
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     """.ping, this is use for testing your ping"""
-    await ctx.send(f'ping: {round(client.latency * 1000)}ms')
+    await ctx.send(f'ping: {round(bot.latency * 1000)}ms')
 
 
-@client.command(aliases=['8ball'])
+@bot.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
     """.8ball <question>, this func will ans your question"""
     replies = ['It is certain.',
@@ -186,21 +200,21 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(replies)}')
 
 
-@client.command(aliases=['stopspam'])
+@bot.command(aliases=['stopspam'])
 async def stop_spam(ctx):
     """.stopspam, this ban people from using the func 'spam'"""
     global allow_spam
     allow_spam = False
 
 
-@client.command(aliases=['allowspam'])
+@bot.command(aliases=['allowspam'])
 async def allow_spam(ctx):
     """.allowspam, this allow people to use the func 'spam'"""
     global allow_spam
     allow_spam = True
 
 
-@client.command()
+@bot.command()
 async def spam(ctx, time, *args):
     """.spam <time> <text>, this func allow people to spam text"""
     if allow_spam:
@@ -211,7 +225,7 @@ async def spam(ctx, time, *args):
                 await ctx.send(text)
 
 
-@client.command(aliases=['rannum'])
+@bot.command(aliases=['rannum'])
 async def ran_num_gen(ctx, start=1, end=2):
     """.rannum <start> <end>, this allow you to generate a random number in a range"""
     try:
@@ -223,7 +237,7 @@ async def ran_num_gen(ctx, start=1, end=2):
         await ctx.send(e)
     
 
-@client.command(aliases=['playguessnum', 'pgn', "guessnum"])
+@bot.command(aliases=['playguessnum', 'pgn', "guessnum"])
 async def guess_num(ctx, num=100):
     global ans, play_guess_num, guess_count
     number = int(num)
@@ -233,13 +247,9 @@ async def guess_num(ctx, num=100):
     await ctx.send("Enter number to guess.")
 
 
-@client.command(aliases=["roastme"])
-async def roast_me(ctx):
-    sentence = get_roast_sent()
-    await ctx.send(sentence)
-
-@client.command()
+@bot.command()
 async def mkpoll(ctx, topic, *opts):
+    # make a poll
     if len(opts) > 10:    
         await ctx.send("The maximum number of options is 10.")
 
@@ -255,13 +265,11 @@ async def mkpoll(ctx, topic, *opts):
         await poll_msg.add_reaction(num_emo_unicode[i])
 
 
-@client.command()
-async def test(ctx):
-    msg = await ctx.send("test for reactions")
-    await msg.add_reaction("1\ufe0f\u20e3")
+def test(var, name):
+    print(f"\n{'-'*30}\n{name}: \n{var}\n{type(var)}\n{'-'*30}\n")
 
 
-@client.command()
+@bot.command()
 async def maybe(ctx):
     num = random.randint(1,2)
     if num == 1:
@@ -270,7 +278,88 @@ async def maybe(ctx):
     else:
         await ctx.send(file=discord.File("nah.gif"))
 
-@client.event
+
+@bot.command()
+async def remind(ctx, *, message):
+    """send reminder after a specific time, default is 1 hour"""
+    if not message:
+        await ctx.send("Invalid input")
+        return
+    #get time and reminder by regex, channel id, and author_id
+    time_match = re.search("^[0-9]+(s|h|d|)", message) #note that this is optional 
+    channel_id = ctx.channel.id
+    author_id = ctx.author.id
+
+    #find the reminder 
+    reminder = message
+    if time_match:
+        if len(message) > len(time_match.group()):
+            reminder = re.sub(time_match.group(), "", message).strip() 
+        else:
+            time_match = None
+    
+    #find time_unit and time_scaler
+    time_unit = "h"
+    time_scaler = 1
+    if time_match:
+        print("We have a time_match!")
+        time_scaler = int(re.search("[0-9]+", time_match.group()).group())
+        time_unit = re.search("(s|h|d)", time_match.group())
+        if time_unit:
+            time_unit = time_unit.group()
+        
+    #tests
+    """
+    test(time_match, "time_match")
+    test(reminder, "reminder")
+    test(time_scaler, "time scaler")
+    test(time_unit, "time_unit")
+    """
+
+    #handle time stuff
+    current_local_time = datetime.datetime.now() 
+    time_dict = {"s": 1, "h": 3600, "d": 86400}
+    wait_time_second = time_dict[time_unit] * time_scaler
+    due_time = current_local_time + datetime.timedelta(seconds=wait_time_second)
+
+    # store the reminder into the "reminder" database
+    cursor.execute("""INSERT INTO reminder 
+        (reminder, receive_time, due_time, channel_id, author_id) 
+        VALUES (?, ?, ?, ?, ?)""", 
+        (reminder, str(current_local_time), str(due_time), channel_id, author_id))
+    conn.commit()
+
+    
+
+@tasks.loop(seconds = 10)
+async def check_reminder():
+    #get data from database
+    raw_data = cursor.execute("""SELECT * FROM reminder""")
+    names = [a[0] for a in raw_data.description]
+    result= []
+    for row in raw_data.fetchall():
+        dict_entry = {}
+        for i in range(len(row)):
+            dict_entry[names[i]] = row[i]
+        result.append(dict_entry)
+
+    # check if due
+    for reminder in result:
+        if parser.parse(reminder["due_time"]) < datetime.datetime.now():
+            #send message
+            channel = bot.get_channel(int(reminder["channel_id"]))
+            author = bot.get_user(int(reminder["author_id"]))
+            await channel.send(f"REMINDER by {author.display_name} on {reminder['receive_time']}:\n{reminder['reminder']}")
+            
+            #remove the ereminder from database
+            cursor.execute("DELETE FROM reminder WHERE reminder = ?", (reminder["reminder"], ))
+            print("\nDELETE REMINDER!!!!!\n")
+            conn.commit()
+            #TODO I should used id, but this will work for most cases and maybe do this later
+
+
+#todo: add task to clear reminder outdated lines periodically (use task loop)
+@bot.event
 async def on_message(msg):
 
     # guess num
@@ -294,10 +383,10 @@ async def on_message(msg):
                     await msg.channel.send(f"You win! You have guessed {guess_count+1} times.")
                 play_guess_num = False
 
-    await client.process_commands(msg)
+    await bot.process_commands(msg)
 
 
-client.run('NjYxNTg0NjUyNTgyMjU2Njgw.XgtiuA.SWtnucLY3NX3Dt9r_DVOMvkAcQY')
+bot.run('NjYxNTg0NjUyNTgyMjU2Njgw.XgtiuA.SWtnucLY3NX3Dt9r_DVOMvkAcQY')
 
 
 
--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-def get_arr(input_list, arr):
-    result_list = []
-    result = None
-    for i, word in enumerate(input_list):
-        if word == arr:
-            for sub_word in input_list[i+1:]:
-                if sub_word.startswith("-"):
-                    break
-                result_list.append(sub_word)
-            result = " ".join(result_list) 
-            return result
+from re import search
+import sqlite3
 
-
-a = get_arr(["-o1", "o1", "o11", "-o2", "o2", "o22", "o23", "-o3"], "-o3")
-print(a)
-print("sdfsdf")
+conn = sqlite3.connect("bot.db")
+c = conn.cursor()
+reminder = {"reminder": "god is dog"}
+r = "lake"
+print(r, type(r))
+c.execute("DELETE FROM reminder WHERE reminder = ?", (r, ))
+conn.commit()
+    
